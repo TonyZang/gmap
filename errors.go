c6d41be4b598375731b010e3d26f506add241b7a
Antony <tonyzhang329@gmail.com>
2024-04-28 22:43:50 +0800

    完善散列桶

--- errors.go
+++ errors.go
@@ -30,16 +30,16 @@ func (ipte IllegalPairTypeError) Error() string {
 	return ipte.msg
 }
 
-type PairRedistributerError struct {
+type PairRedistributorError struct {
 	msg string
 }
 
-func newPairRedistributorError(errMsg string) PairRedistributerError {
-	return PairRedistributerError{
+func newPairRedistributorError(errMsg string) PairRedistributorError {
+	return PairRedistributorError{
 		msg: fmt.Sprintf("concurrent map: failing pair redistribution: %s", errMsg),
 	}
 }
 
-func (pre PairRedistributerError) Error() string {
+func (pre PairRedistributorError) Error() string {
 	return pre.msg
 }
--- segment.go
+++ segment.go
@@ -1,4 +1,23 @@
 package gmap
 
 type Segment interface {
+	Put(p Pair) (bool, error)
+
+	Get(key string) Pair
+
+	GetWithHash(key string, keyHash uint64) Pair
+
+	Delete(key string) bool
+
+	Size() uint64
+}
+
+type segment struct {
+	buckets []Bucket
+
+	bucketsLen int
+
+	pairTotal uint64
+
+	pairRedistributor PairRedistributor
 }
