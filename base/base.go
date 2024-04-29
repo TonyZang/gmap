d253c3a721dafc230c8f627e9d1cc9a22c03c3dd
Antony <tonyzhang329@gmail.com>
2024-04-29 10:45:06 +0800

    添加散列段细节

--- base/base.go
+++ base/base.go
@@ -1,9 +1,14 @@
 package base
 
 const (
+	// 默认的装载因子
+	// 当前散列段中的某个散列桶的尺寸超过了
+	// 本因子与散列段尺寸的乘积，就会触发再散列
 	DEFAULT_BUCKET_LOAD_FACTOR float64 = 0.75
-	DEFAULT_BUCKET_NUMBER      int     = 16
-	DEFAULT_BUCKET_MAX_SIZE    uint64  = 1000
+	// 散列桶的默认数量
+	DEFAULT_BUCKET_NUMBER int = 16
+	// 单个散列桶的默认最大尺寸
+	DEFAULT_BUCKET_MAX_SIZE uint64 = 1000
 )
 
 const (
--- segment.go
+++ segment.go
@@ -1,5 +1,13 @@
 package gmap
 
+import (
+	"fmt"
+	"gmap/base"
+	"gmap/util"
+	"sync"
+	"sync/atomic"
+)
+
 type Segment interface {
 	Put(p Pair) (bool, error)
 
@@ -13,11 +21,96 @@ type Segment interface {
 }
 
 type segment struct {
+	// 散列桶切片
 	buckets []Bucket
 
+	// 散列桶切片长度
 	bucketsLen int
 
+	// 键值对总数
 	pairTotal uint64
 
+	// 键值对的再分布器
 	pairRedistributor PairRedistributor
+
+	lock sync.Mutex
+}
+
+func newSegment(
+	bucketNumber int, pairRedistributor PairRedistributor) Segment {
+	if bucketNumber <= 0 {
+		bucketNumber = base.DEFAULT_BUCKET_NUMBER
+	}
+	if pairRedistributor == nil {
+		pairRedistributor = newDefaultPairRedistributor(base.DEFAULT_BUCKET_LOAD_FACTOR, bucketNumber)
+	}
+	buckets := make([]Bucket, bucketNumber)
+	for i := 0; i < bucketNumber; i++ {
+		buckets[i] = newBucket()
+	}
+	return &segment{
+		buckets:           buckets,
+		bucketsLen:        bucketNumber,
+		pairRedistributor: pairRedistributor,
+	}
+}
+
+func (s *segment) Put(p Pair) (bool, error) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	b := s.buckets[int(p.Hash()%uint64(s.bucketsLen))]
+	ok, err := b.Put(p, nil)
+	if ok {
+		newTotal := atomic.AddUint64(&s.pairTotal, 1)
+		s.redistribe(newTotal, b.Size())
+	}
+	return ok, err
+}
+
+func (s *segment) Get(key string) Pair {
+	return s.GetWithHash(key, util.Hash(key))
+}
+
+func (s *segment) GetWithHash(key string, keyHash uint64) Pair {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	b := s.buckets[int(keyHash%uint64(s.bucketsLen))]
+	return b.Get(key)
+}
+
+func (s *segment) Delete(key string) bool {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	b := s.buckets[int(util.Hash(key)%uint64(s.bucketsLen))]
+	ok := b.Delete(key, nil)
+	if ok {
+		newTotal := atomic.AddUint64(&s.pairTotal, ^uint64(0))
+		s.redistribe(newTotal, b.Size())
+	}
+	return ok
+}
+
+func (s *segment) Size() uint64 {
+	return atomic.LoadUint64(&s.pairTotal)
+}
+
+func (s *segment) redistribe(pairTotal uint64, bucketSize uint64) (err error) {
+	defer func() {
+		if p := recover(); p != nil {
+			if pErr, ok := p.(error); ok {
+				err = newPairRedistributorError(pErr.Error())
+			} else {
+				err = newPairRedistributorError(fmt.Sprintf("%s", p))
+			}
+		}
+	}()
+
+	s.pairRedistributor.UpdateThreshold(pairTotal, s.bucketsLen)
+	bucketStatus := s.pairRedistributor.CheckBucketStatus(pairTotal, bucketSize)
+	newBuckets, changed := s.pairRedistributor.Redistribe(bucketStatus, s.buckets)
+	if changed {
+		s.buckets = newBuckets
+		s.bucketsLen = len(s.buckets)
+	}
+	return nil
 }
